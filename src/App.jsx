6fabdb2327ab1bb64258e0d9ec0e2511ff16bc2f
UserVKO2 <medet.uk@gmail.com>
2025-06-24 03:56:04 +0600

    Добавил стили module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.module.css'
+import styles from './App.module.css'
 
 function App() {
   return (
     <>
-      <h1 className='text-4xl text-center pt-20'>App component</h1>
+      <div className={`${styles.wrapper} text-center`}>
+        <h1 className='text-4xl pt-20 text-amber-50'>App component</h1>
+        <p className={styles.paragraf}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Commodi vitae architecto ducimus praesentium recusandae 
+          quam maxime obcaecati amet ipsa dolor. Inventore reiciendis 
+          doloribus, nesciunt facere pariatur at possimus nulla. 
+        </p>
+      </div>
     </>
   )
 }
